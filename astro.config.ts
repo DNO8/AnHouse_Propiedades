a60e5c93069057ac1fcaae0c3854c4d1d18fd010
DANO <dcontrerasl@live.com>
2025-08-30 02:24:40 -0400

    agregando sitemap para mejorar el SEO y la busqueda en Google

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://www.anhousepropiedades.cl',
   vite: {
     plugins: [tailwindcss()],
     resolve:{
