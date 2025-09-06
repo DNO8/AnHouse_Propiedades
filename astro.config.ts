d25b2675e3e89ce905985ddc60a068c38a93550e
DANO <dcontrerasl@live.com>
2025-09-06 02:03:20 -0400

    quitamos los test

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,7 @@
 // @ts-check
-
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, envField } from "astro/config";
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.anhousepropiedades.cl",
